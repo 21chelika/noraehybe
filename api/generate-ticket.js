0f34f37ecd8d96f6c486ca9308e027a4867172c6
Chelika Patrcia <85401286+21chelika@users.noreply.github.com>
2025-11-01 06:10:19 +0700

    Update generate-ticket.js

--- api/generate-ticket.js
+++ api/generate-ticket.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
 
     // === ✉️ Data email dasar ===
     const RESEND_API_KEY = process.env.RESEND_API_KEY;
-    const RESEND_FROM = process.env.RESEND_FROM || "NORAEHYBE <onboarding@resend.dev>";
+    const RESEND_FROM = process.env.RESEND_FROM || "onboarding@resend.dev";
     if (!RESEND_API_KEY) {
       console.error("❌ RESEND_API_KEY missing");
       return res.status(500).json({ error: "Missing Resend API key" });
@@ -205,3 +205,4 @@ async function appendToSheet(row) {
 
   console.log("✅ Data appended to Google Sheets");
 }
+
